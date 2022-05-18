d251fb81ab17061e4c83ed167374027d008fea45
ryo-hidaka <66785120+ryo-hidaka@users.noreply.github.com>
2022-05-18 13:38:51 +0900

    CSS準備

--- src/App.js
+++ src/App.js
@@ -1,36 +1,11 @@
-import { useState, useCallback, useMemo } from "react";
+import React from "react";
+import { InlineStyle } from "./components/InlineStyle";
 import "./styles.css";
-import { ChildArea } from "./ChildArea";
 
 export default function App() {
-  console.log("App");
-  const [text, setText] = useState("");
-  const [opne, setOpen] = useState(false);
-
-  const onChangeText = (event) => {
-    setText(event.target.value);
-  };
-
-  const onClickOpne = () => {
-    setOpen(!opne);
-  };
-
-  const onClickClose = useCallback(() => {
-    setOpen(false);
-  }, [setOpen]);
-
-  const temp = useMemo(() => {
-    return 1 + 3;
-  }, []);
-
-  console.log(temp);
   return (
     <div className="App">
-      <input value={text} onChange={onChangeText} />
-      <br />
-      <br />
-      <button onClick={onClickOpne}>表示</button>
-      <ChildArea open={opne} onClickClose={onClickClose} />
+      <InlineStyle />
     </div>
   );
 }
